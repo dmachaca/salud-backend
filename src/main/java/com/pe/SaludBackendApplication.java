0676ca66651f01fb7a59a0522ad220eb24221e4c
dmachaca <densymachaca@gmail.com>
2026-04-19 20:22:32 -0500

    feat: proyecto base Spring Boot salud-backend

---
+++ src/main/java/com/pe/SaludBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludBackendApplication.class, args);
+	}
+
+}
